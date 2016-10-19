c0340818eda84fb476833c6dea9e1a135a768b0b
Sondre Kolaautomat <sondre.kolaautomat@gmail.com>
2016-10-18 22:09:41 -0300

    Google Apps script

---
+++ google.apps/Helper.js
@@ -0,0 +1,45 @@
+function onOpen() {
+  var ui = DocumentApp.getUi();
+  ui.createAddonMenu()
+      .addItem('Insert link with title', 'insertLinkWithTitle')
+      .addToUi();
+
+}
+
+function insertLinkWithTitle() {
+  var cursor = DocumentApp.getActiveDocument().getCursor();
+  if (cursor) {
+    var ui = DocumentApp.getUi();
+    var response = ui.prompt('Automatic Title', 'Link', ui.ButtonSet.OK_CANCEL);  
+    
+    // Process the user's response.
+    if (response.getSelectedButton() == ui.Button.OK) {
+      Logger.log('The user\'s name is %s.', response.getResponseText());
+      var url = response.getResponseText();
+      // if response is OK....
+      var httpResponse = UrlFetchApp.fetch("http://api.sonkol.ml/v1/title-from-url?url=" + url);
+      var json = httpResponse.getContentText();
+      var data = JSON.parse(json);
+      
+      if(data.error) {
+        DocumentApp.getUi().alert('Error retrieving page.');
+      } else {
+        var title = data.title;
+        
+        var element = cursor.insertText(title);
+        
+        if(element) {
+          element.setLinkUrl(url);
+        } else {
+          DocumentApp.getUi().alert('Cannot insert text at this cursor location.');        
+        }
+      }
+    } else if (response.getSelectedButton() == ui.Button.CANCEL) {
+      Logger.log('Link insertion cancelled.');
+    } else {
+      Logger.log('The user clicked the close button in the dialog\'s title bar.');
+    }
+  } else {
+      DocumentApp.getUi().alert('Cannot find a cursor in the document.');
+  }
+}
